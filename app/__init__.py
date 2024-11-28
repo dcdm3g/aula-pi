74c064edb773d3686d6d9867bd70dedb6a44be1f
Davi Melo <davicosta01082008@gmail.com>
2024-11-28 15:48:11 -0300

    feat: add get event route

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.routes.login import login_bp
 from app.routes.refresh_session import refresh_session_bp
 from app.routes.add_event import add_event_bp
 from app.routes.get_events import get_events_bp
+from app.routes.get_event import get_event_bp
 from app.routes.evaluate_event import evaluate_event_bp
 from app.routes.revoke_event import revoke_event_bp
 from app.routes.deposit_into_wallet import deposit_into_wallet_bp
@@ -27,6 +28,7 @@ def create_app():
   app.register_blueprint(login_bp)
   app.register_blueprint(add_event_bp)
   app.register_blueprint(get_events_bp)
+  app.register_blueprint(get_event_bp)
   app.register_blueprint(evaluate_event_bp)
   app.register_blueprint(revoke_event_bp)
   app.register_blueprint(deposit_into_wallet_bp)
